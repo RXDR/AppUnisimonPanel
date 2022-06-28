32f28238986f57e65831b659a8269cc76c368759
Ruben Araujo <ruben.araujo@unisimon.edu.co>
2022-06-28 15:09:38 -0500

    subiendo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,28 +5,7 @@ module.exports = {
   theme: {
 
     extend: {},
-    screens: {
-      'sm': '360px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
-    fontSize: {
-      sm: ['12px', '20px'],
-      base: ['16px', '24px'],
-      lg: ['20px', '28px'],
-      xl: ['24px', '32px'],
-    },
+   
   },
   plugins: [],
 }
